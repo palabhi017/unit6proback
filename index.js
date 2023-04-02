bfbb556b590e41bdd0d98dc2d6b00dedd2b99c68
palabhi017 <palabhi017@gmail.com>
2023-04-02 15:50:19 +0530

    new backend

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const {productRouter} =  require("./Routes/product.route")
 const {cartRouter} =  require("./Routes/cart.route")
 const {orderRouter} =  require("./Routes/order.route")
 
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.mongoURL);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+
 app.use(cors())
 app.use(express.json())
 app.use("/user",userRouter)
@@ -17,12 +27,16 @@ app.use("/order",orderRouter)
 
 
 
-
-app.listen(process.env.port,async()=>{
-try {
-    await connection
-    console.log("port is running")
-} catch (error) {
-    console.log(error)
-} 
+connectDB().then(() => {
+    app.listen(process.env.port, () => {
+        console.log("listening for requests");
+    })
 })
+// app.listen(process.env.port,async()=>{
+// try {
+//     await connection
+//     console.log("port is running")
+// } catch (error) {
+//     console.log(error)
+// } 
+// })
